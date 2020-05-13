85ed1a41459c875caa138d7893b4457d83afdd94
huyanhtran <trananhhuy1991@gmail.com>
2020-05-13 22:54:52 +0700

    add Time/TimeStop/DisableButton

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // 1. computer need to pick a random number
 let computerNum = Math.floor(Math.random()*100) + 1
 let historyNum = []
-let time = 5
-let myTime;
+let time = 20
+let myTime
 console.log("This is the number that computer chose: ", computerNum)
 
 // 2. when user click the guess, it will fire a function
@@ -12,19 +12,24 @@ function guessNumber(){
     let userNum = document.getElementById("guessNumber").value
     let resultMessage = " "
     // 4. compare with the value computer picked with user value
-    if(computerNum > userNum){              // 5. if computer's num > user's num => Print: Too low
-        resultMessage = "too low"
-    } else if(computerNum < userNum){        // 6. if computer's num < user's num => Print: Too high
-        resultMessage = "too high"
-    } else if(computerNum == userNum){       // 7. if computer's num === user's num => Print: Correct
+    if(userNum < 0 || userNum > 100){
+        disabledButton()
+        alert("You lose!")
+    } else if(computerNum > userNum){              
+        resultMessage = "Too low"
+    } else if(computerNum < userNum){        
+        resultMessage = "Too high"
+    } else if(computerNum == userNum){     
         resultMessage = "Correct"
     }
-    
-    if(historyNum.length < 5){
-        historyNum.push(userNum)
-    } else{
-        alert("Can not guess now. Reset the game!")
 
+    if(historyNum.length < 4){
+        historyNum.push(userNum)
+    } else {
+        historyNum.push(userNum)
+        document.getElementById("turnCount").innerHTML = `Out of turns`
+        timeOut()
+        disabledButton()
     }
     // keep the history
     document.getElementById("resultArea").innerHTML = `${resultMessage}` // 8. show the result to user
@@ -35,25 +40,33 @@ function guessNumber(){
 function timeCounting(){
     myTime = setInterval(() => {
         time -= 1
-        if(time == -1){
-            alert("Time over!")
+        document.getElementById("timeCount").innerHTML = `Time left: ${time} second`
+        if(time <= 0){
+            document.getElementById("timeOver").innerHTML = "Time Over!"
+            timeOut()
+            disabledButton()
         }
-        document.getElementById('timeCount').innerHTML = `HURRY UP !!: ${time} second`
     }, 1000)// every 1 second, it will add 1 into time variable (computer use millisecond so 1000 is 1 second)
 }
 timeCounting()// fire the timecounting function!!
-
 
 //Timeout 
 function timeOut() {
     clearInterval(myTime);
 }
 
+// Reset feature (when click the reset button = > reset everything)
+function resetEverything(){
+    // do something
+    document.getElementById("guessNumber").reset()
+}
 
-// 1. make the chance feature (only have 5 chance)
-// 2. reset feature (when click the reset button = > reset everything)
-// 3. if user win/lose => the guess button will be disabled(cannot click)
-// 4. add timer feature (if you cannot finish game within 20s, then you lose)
+// If user win/lose => the guess button will be disabled(cannot click)
+function disabledButton(){
+    // do something
+    document.getElementById("buttonGuess").disabled = true
+}
+
 
 
 
